39aae128ada98f2f75f5683237c37546a66a2616
soyangelmtz <angeld.mtzg@gmail.com>
2025-01-11 12:25:48 -0600

    hope this works

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ function page() {
   return (
     <main className='w-screen h-screen flex p-3'>
       <div className='w-fit h-fit max-h-[90vh] m-auto flex overflow-hidden rounded-lg border'>
-        <img src="/invita.jpeg" className='w-full h-full object-contain ' alt="" />
+        <img src="/invitacion.jpeg" className='w-full h-full object-contain ' alt="" />
       </div>
     </main>
   )
