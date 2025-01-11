165fa7c218d5ae16ea620d4834ec8e42c604e06e
soyangelmtz <angeld.mtzg@gmail.com>
2025-01-11 12:34:58 -0600

    responsive img

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <main className='w-screen h-screen flex p-3'>
-      <div className='w-fit h-fit max-h-[90vh] m-auto flex overflow-hidden rounded-lg border'>
+      <div className='w-fit h-fit max-h-[90vh] md:aspect-[16/9] m-auto flex overflow-hidden rounded-lg '>
         <img src="/invitacion.jpeg" className='w-full h-full object-contain ' alt="" />
       </div>
     </main>
